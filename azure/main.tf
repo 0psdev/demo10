efe864f1b44647dd4185d22e4b389ebbeaea37b9
OpsDev <ops2dev@hotmail.com>
2025-03-04 00:29:05 +0700

    Update comment

--- azure/main.tf
+++ azure/main.tf
@@ -1,15 +1,16 @@
+//Resource Group
 resource "azurerm_resource_group" "rsg_names" {
     name     = var.rsg_names
     location = var.location_names
 }
-
+//Virtual Network
 resource "azurerm_virtual_network" "vnetwork" {
   name = var.vnet_name
   resource_group_name = var.rsg_names
   location = var.location_names
   address_space = var.vnet_subnet
 }
-
+//Subnet
 resource "azurerm_subnet" "snet" {
   for_each = toset(var.snet_names)
   name = each.key
@@ -17,7 +18,7 @@ resource "azurerm_subnet" "snet" {
   virtual_network_name = azurerm_virtual_network.vnetwork.name
   address_prefixes = var.snet_subnets[each.key]
 }
-
+//Public IP
 resource "azurerm_public_ip" "my_public_ip" {
   name                = "public-ip-nat"
   location            = azurerm_resource_group.rg.location
@@ -25,23 +26,23 @@ resource "azurerm_public_ip" "my_public_ip" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
-
+//NAT Gateway
 resource "azurerm_nat_gateway" "nat_gateway" {
   name                = var.natgw_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 }
-
+//NAT Gateway Public IP Association
 resource "azurerm_nat_gateway_public_ip_association" "ass_public_ip" {
   nat_gateway_id       = azurerm_nat_gateway.my_nat_gateway.id
   public_ip_address_id = azurerm_public_ip.my_public_ip.id
 }
-
+//NAT Gateway Subnet Assoication
 resource "azurerm_subnet_nat_gateway_association" "ass_subnet_nat" {
   subnet_id      = azurerm_subnet.snet["subnet_02"].id
   nat_gateway_id = azurerm_nat_gateway.my_nat_gateway.id
 }
-
+//NIC vm
 resource "azurerm_network_interface" "nic-vm" {
   for_each = toset(var.vm_names)
   name                = "${each.key}-nic"
@@ -54,14 +55,14 @@ resource "azurerm_network_interface" "nic-vm" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-
+//NSG
 resource "azurerm_network_security_group" "nsg-vm" {
   for_each = toset(var.vm_names)
   name                = "${each.key}-nsg"
   resource_group_name = var.rsg_names
   location            = var.location_names
 }
-
+//NSG Rule
 resource "azurerm_network_security_rule" "rule_vm_1" {
   for_each = toset(var.vm_names)
   name                       = "allow_icmp"
@@ -76,13 +77,13 @@ resource "azurerm_network_security_rule" "rule_vm_1" {
   resource_group_name = var.rsg_names
   network_security_group_name = azurerm_network_security_group.nsg-vm[each.key].name
 }
-
+//NSG Rule Associate
 resource "azurerm_network_interface_security_group_association" "nsg-ass-vm" {
   for_each = toset(var.vm_names)
   network_interface_id      = azurerm_network_interface.nic-vm[each.key].id
   network_security_group_id = azurerm_network_security_group.nsg-vm[each.key].id
 }
-
+//Virtual Machine
 resource "azurerm_windows_virtual_machine" "vm" {
   for_each = toset(var.vm_names)
   name                = "${each.key}"
